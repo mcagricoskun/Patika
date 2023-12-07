96036ad0b1551c9fb41cef1c5ff7de8d3729aa1c
mcagricskkn <104147659+mcagricskkn@users.noreply.github.com>
2023-12-07 12:14:07 +0300

    Daire

--- src/Daire.java
+++ src/Daire.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 
 public class Daire {
     public static void main(String[] args) {
-        double yariCap, alan, cevre, pi=3.14;
+        double r, alan, cevre, pi=3.14;
 
         Scanner input = new Scanner(System.in);
         System.out.println("Yarıçapı girin: ");
-        yariCap = input.nextDouble();
+        r = input.nextDouble();
 
-        alan = pi*yariCap*yariCap;
-        cevre = 2 * pi * yariCap;
+        alan = pi*r*r;
+        cevre = 2 * pi * r;
 
         System.out.println("Alan= " + alan);
         System.out.println("Çevre= " + cevre);
