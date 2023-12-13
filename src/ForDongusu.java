856ed7e5ba3b3e7e1181306afcd421076026e531
mcagricskkn <104147659+mcagricskkn@users.noreply.github.com>
2023-12-13 14:21:27 +0300

    For Dongusu

---
+++ src/ForDongusu.java
@@ -0,0 +1,13 @@
+public class ForDongusu {
+    public static void main(String[] args) {
+        // for (durum 1; durum 2; durum 3) {
+        //  çalışacak kod bloğu
+        // }
+        //Başlangıçı ve bitişini bildiğimiz durumlarda kullanılır
+        //While genelde sonu bilinmeyen döngülerde kullanılır
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
