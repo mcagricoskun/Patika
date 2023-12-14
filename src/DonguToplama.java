44c1667500443ebc5a9ff4e5b6e72a6cf1e00980
mcagricskkn <104147659+mcagricskkn@users.noreply.github.com>
2023-12-14 16:56:21 +0300

    girilen negatif sayıların toplamı

---
+++ src/DonguToplama.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DonguToplama {
+    public static void main(String[] args) {
+        int number, toplam = 0;
+
+        Scanner input = new Scanner(System.in);
+        do {
+            System.out.println("Bir sayı giriniz: ");
+            number = input.nextInt();
+            if (number % 2 == 1) {
+                toplam = number + toplam;
+
+            }
+        }
+        while (number > 0) ;
+        System.out.println("Girdiğiniz tek sayıların toplamı= " + toplam);
+
+
+    }
+}
+
