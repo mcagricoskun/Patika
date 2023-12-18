43a195299d9ce7d04725259aa75566ee3507e6d0
mcagricskkn <104147659+mcagricskkn@users.noreply.github.com>
2023-12-18 12:11:11 +0300

    Armstrong

---
+++ src/DonguArmstronV2.java
@@ -0,0 +1,23 @@
+public class DonguArmstronV2 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 500000; i++) {
+            int b=i;
+            int basamakSayisi=0;
+            int temp=i;
+            int toplam=0;
+            while (b != 0) {
+                b = b / 10;
+                basamakSayisi++;
+            }
+            for(int y =1; y<=basamakSayisi; y++){
+                int c = temp % 10;
+                temp /= 10;
+                toplam = (int) (toplam + Math.pow(c,basamakSayisi));
+            }
+            if(toplam == i){
+                System.out.println(i);
+            }
+        }
+    }
+}
