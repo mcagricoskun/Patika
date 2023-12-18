67bdc99861ead951b9d3901b0443307bcbe74edb
mcagricskkn <104147659+mcagricskkn@users.noreply.github.com>
2023-12-18 16:36:00 +0300

    yıldız

---
+++ src/Dongu/YıldızlarSekil.java
@@ -0,0 +1,27 @@
+package Dongu;
+
+import java.util.Scanner;
+
+public class YıldızlarSekil {
+
+    /*
+      bb*bb
+      b***b
+      *****
+     */
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz :");
+        int n = klavye.nextInt();
+
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
