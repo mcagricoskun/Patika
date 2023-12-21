8f7971fc3b0b775f09a4733021aa48736e0e54df
mcagricskkn <104147659+mcagricskkn@users.noreply.github.com>
2023-12-21 15:05:04 +0300

    Hesap Makinesi

---
+++ src/Method/RecursiveUsluSayi.java
@@ -0,0 +1,25 @@
+package Method;
+
+import java.util.Scanner;
+
+public class RecursiveUsluSayi {
+    static int uslusayi(int taban, int us) {
+        if (us == 0) {
+            return 1;
+        }
+        else {
+            return taban * uslusayi(taban, us - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Taban değeri: ");
+        int taban = input.nextInt();
+        System.out.println("Üs değeri: ");
+        int us = input.nextInt();
+        int result = uslusayi(taban, us);
+        System.out.println(taban + " üzeri " + us + " eşittir= " + result);
+    }
+}
+
